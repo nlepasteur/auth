7ec7ce49c45f21eee82bae7f4071e313379cbe98
Nicolas Le Pasteur <nicolas.lepasteur@gmail.com>
2020-05-27 18:34:33 +0200

    bcrypt & joi added

--- server/auth/index.js
+++ server/auth/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const Joi = require('@hapi/joi');
+const bcrypt = require('bcryptjs');
 
 const db = require('../db/connection');
 const users = db.get('users');
@@ -15,7 +16,7 @@ const schema = Joi.object({
     .min(2)
     .max(30)
     .required(),
-  password: Joi.string().min(6).required(),
+  password: Joi.string().trim().min(6).required(),
 });
 
 const router = express.Router();
@@ -26,13 +27,34 @@ router.get('/', (req, res) => {
   });
 });
 
-router.post('/signup', (req, res) => {
-  createIndexes('usernames');
+router.post('/signup', async (req, res, next) => {
   console.log('enter post signup middleware');
   const result = schema.validate(req.body);
   //const { error, value } = schema.validate(req.body);
+  if (Object.keys(result).length == 1) {
+    const user = await users.findOne({ username: req.body.username });
+    if (!user) {
+      const hashedPassword = await bcrypt.hash(req.body.password, 12);
 
-  res.json(result);
+      const newUser = {
+        username: req.body.username,
+        password: hashedPassword,
+      };
+
+      const insertedUser = await users.insert(newUser);
+      delete newUser.password;
+      res.json(insertedUser);
+
+      // res.json({ hashedPassword }); // res.json({ hashedPassword }); // retenir cette syntaxe qui revient à écrire { hashedPassword: hashedPassword }
+    } else {
+      const error = new Error('This username already exists');
+      next(error);
+    }
+    // res.json({ user }); // retenir cette syntaxe qui revient à écrire { user: user }
+    // res.json(result);
+  } else {
+    next(result.error);
+  }
 });
 
 module.exports = router;
--- server/db/connection.js
+++ server/db/connection.js
@@ -1,4 +1,5 @@
 const monk = require('monk');
+
 const db = monk('localhost/auth-for-noobs');
 
 module.exports = db;
